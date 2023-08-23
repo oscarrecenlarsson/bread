b350a1854a6a34f9baf8a3ec030122ad94f0492b
Oscar Recén Larsson <112632742+oscarrecenlarsson@users.noreply.github.com>
2023-08-23 17:33:34 +0200

    first controller function working

---
+++ api/controllers/shipmentController.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+
+function createAndBroadcastShipment(logisticsBC, req, res) {
+  //skapa en ny transaction på aktuell node
+  //behöver jag göra om denna för att det ska fungera med "move"????
+
+  //if body har ett helt shipmentobject sett shipment till det annars skapa en ny shipment:
+  // console.log(req.body);
+
+  const shipment = logisticsBC.createShipment(
+    req.body.route,
+    req.body.products
+  );
+
+  //Lägg till nya transaktioner till aktuell node
+  logisticsBC.addShipmentToPendingList(shipment);
+
+  logisticsBC.addShipmentToProcessAndSend(shipment);
+
+  //iterera igenom alla nätverksnoder i networkNodes och nropa reskpektive och skcika över den nya transaktionen
+  // behöver vi använda axios för att göra ett post anrop
+  //await axios.post(url,body)
+
+  //anropa api/transaction för alla network nodes
+
+  logisticsBC.networkNodes.forEach(async (url) => {
+    // const body = { transaction: transaction };
+
+    await axios.post(`${url}/api/node/shipment`, shipment);
+  });
+
+  res.status(201).json({
+    sucess: true,
+    data: "shipment has been created and broadcasted to the network",
+  });
+}
+
+module.exports = { createAndBroadcastShipment };
--- api/routes/network.js
+++ api/routes/network.js
@@ -2,6 +2,9 @@ const axios = require("axios");
 const express = require("express");
 const router = express.Router();
 const node = require("./node");
+const {
+  createAndBroadcastShipment,
+} = require("../controllers/shipmentController");
 
 // const logisticsBC = require("../server").app.locals.logisticsBC;
 
@@ -37,38 +40,7 @@ module.exports = function (logisticsBC) {
   });
 
   router.post("/shipment", (req, res) => {
-    //skapa en ny transaction på aktuell node
-    //behöver jag göra om denna för att det ska fungera med "move"????
-
-    //if body har ett helt shipmentobject sett shipment till det annars skapa en ny shipment:
-    // console.log(req.body);
-
-    const shipment = logisticsBC.createShipment(
-      req.body.route,
-      req.body.products
-    );
-
-    //Lägg till nya transaktioner till aktuell node
-    logisticsBC.addShipmentToPendingList(shipment);
-
-    logisticsBC.addShipmentToProcessAndSend(shipment);
-
-    //iterera igenom alla nätverksnoder i networkNodes och nropa reskpektive och skcika över den nya transaktionen
-    // behöver vi använda axios för att göra ett post anrop
-    //await axios.post(url,body)
-
-    //anropa api/transaction för alla network nodes
-
-    logisticsBC.networkNodes.forEach(async (url) => {
-      // const body = { transaction: transaction };
-
-      await axios.post(`${url}/api/node/shipment`, shipment);
-    });
-
-    res.status(201).json({
-      sucess: true,
-      data: "shipment has been created and broadcasted to the network",
-    });
+    createAndBroadcastShipment(logisticsBC, req, res);
   });
 
   router.patch("/shipment", (req, res) => {
