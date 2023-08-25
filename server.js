a1b92bc5505c92514171994a4ae8d31a26aa32dc
Oscar Recén Larsson <112632742+oscarrecenlarsson@users.noreply.github.com>
2023-08-25 20:17:55 +0200

    server.js cleanup

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 const express = require("express");
-const { v4: uuidv4 } = require("uuid");
-
 const Blockchain = require("./blockchain");
-const { default: axios } = require("axios");
-const app = express();
-
 const network = require("./api/routes/network");
 const node = require("./api/routes/node");
 
+const app = express();
 const logisticsBC = new Blockchain();
-// app.locals.logisticsBC = logisticsBC;
-console.log(logisticsBC);
-
 const PORT = process.argv[2];
-// const nodeAddress = uuidv4().split("-").join(""); //mining
 
-// Middleware...
+// middleware
 app.use(express.json());
 
 app.use("/api/network", network(logisticsBC));
